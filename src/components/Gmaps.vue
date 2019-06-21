d0795d85f27c14f0e1456fc892c6a184b5886d61
pandr01 <paulo.andrade@toka.mx>
2019-06-21 18:10:33 -0500

    Creando proyecto base

---
+++ src/components/Gmaps.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="map">
+
+  </div>
+</template>
+
+<script>
+// Importamos la configuración de google Maps
+import gmapsInit from '@/utils/gmaps'
+
+export default {
+  name: "Gmaps",
+  data: () => ({
+
+  }),
+  async mounted(){
+    try {
+      // Cargamos las dependencias
+      const google = await gmapsInit()
+      // Inicializamos google maps
+      const map = new google.maps.Map(document.getElementById('map'),{
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      })
+
+      /*
+      // const geocoder = new google.maps.Geocoder()
+      geocoder.geocode({ address: 'Austria'}, (results, status) => {
+        if(status !== 'OK' || !results[0]) {
+          throw new Error(status)
+        }
+
+        map.setCenter(results[0].geometry.location)
+        map.fitBounds(results[0].geometry.viewport)
+      })
+      */
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#map {
+  width: 100%;
+  height: 400px;
+}
+</style>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Demo google maps</h1>
+
+    <Gmaps></Gmaps>
+  </div>
+</template>
+
+<script>
+import Gmaps from '../components/Gmaps.vue'
+
+export default {
+  name: 'inicio',
+  components: {
+    Gmaps
+  },
+  methods: {
+  },
+  computed: {
+  },
+  created(){
+  }
+}
+</script>
